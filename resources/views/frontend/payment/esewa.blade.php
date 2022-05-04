9ebb7a45b335e571a36206ea28421ab0037a6a99
sujan-sht <sujanstha016@gmail.com>
2022-05-04 14:06:36 +0545

    to pull

--- resources/views/frontend/payment/esewa.blade.php
+++ resources/views/frontend/payment/esewa.blade.php
@@ -13,8 +13,8 @@
       <input value="0" name="pdc" type="hidden">
       <input value="{{$esewa->esewa_key}}" name="scd" type="hidden">
       <input value="{{$ordercode->code}}" name="pid" type="hidden">
-      <input value="https://tibet-collectives.nextnepal.org/page/esewa_payment_success?q=su" type="hidden" name="su">
-      <input value="https://tibet-collectives.nextnepal.org/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
+      <input value="http://localhost:8000/page/esewa_payment_success?q=su" type="hidden" name="su">
+      <input value="http://localhost:8000/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
       <input value="Submit" class ='submitesewa' type="submit" style= "display:none">
       </form>
     <script> 
